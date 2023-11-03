af5941ca056920124fa43d9a12af5a2635fda7e8
Devandra <devandra271203@gmail.com>
2023-11-03 16:39:05 +0700

    Tugas 7 Bug fix

--- lib/option.dart
+++ lib/option.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatelessWidget {
         return Scaffold(
             appBar: AppBar(
               title: const Text(
-                'Shopping List',
+                "Evan's Inventory",
               ),
               backgroundColor: Colors.amber,
             ),
@@ -38,7 +38,7 @@ class MyHomePage extends StatelessWidget {
                       padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                       // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
                       child: Text(
-                        'PBP Shop', // Text yang menandakan toko
+                        'Inventory Option', // Text yang menandakan toko
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontSize: 30,
